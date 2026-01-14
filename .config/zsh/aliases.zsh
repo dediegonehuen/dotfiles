53c2cf8f310b7aa055290987bca33532a70a6e92
Arstotzko <52691928+dediegonehuen@users.noreply.github.com>
2026-01-14 11:49:09 -0300

    refactor: reorganize and clean up zsh aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,26 +1,30 @@
 # Shortcut
+alias c='clear'
+alias h='history'
 alias k="kubectl"
 alias h="helm"
 alias tf="terraform"
 alias a="ansible"
 alias ap="ansible-playbook"
 
+# Listing
+alias ll='ls -ltrh'
+alias la='ls -rthA'
+alias l='ls -rthal'
+
 # Visual studio code
 alias code="open -a 'Visual Studio Code'"
 
-## General propouse
-alias dns-prune-cache="sudo systemctl restart systemd-resolved"
 
-## Ls aliases
-alias ll='ls -l'
-alias la='ls -A'
-alias l='ls -CF'
+# General propouse
 alias grep='grep --color=auto'
+
 if [ -f /usr/bin/batcat ]; then
   alias cat='batcat --style=plain --paging=never'
 elif [ -f /usr/bin/bat ]; then
   alias cat='bat --style=plain --paging=never'
 fi
+
 if [ -f /usr/bin/exa ]; then
   alias ls='exa --group-directories-first'
   alias tree='exa -T'
